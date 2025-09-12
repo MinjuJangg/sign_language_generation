b9b3bc1040d56c4f3a0a4ca5a16e0f5d103fd760
MinjuJangg <jmj1004@dongguk.edu>
2025-09-12 20:51:08 +0900

    bash

--- run_stage2.sh
+++ run_stage2.sh
@@ -1,19 +1,23 @@
 
-accelerate launch --gpu_ids 2 --main_process_port=29700 --num_processes=1 --use_deepspeed --mixed_precision="fp16"  stage2_train_inpaint_model.py \
+accelerate launch --gpu_ids 1 --main_process_port=29700 --num_processes=1 --use_deepspeed --mixed_precision="fp16"  stage2_train_only_previous.py \
   --pretrained_model_name_or_path="stabilityai/stable-diffusion-2-1-base" \
   --image_encoder_p_path='facebook/dinov2-giant' \
-  --json_path='/home/minju/PCDMs1/data/sample.json' \
+  --image_encoder_g_path='laion/CLIP-ViT-H-14-laion2B-s32B-b79K' \
+  --json_path='/home/minju/PCDMs1/data/sample_source.json' \
   --image_root_path="/home/minju/PCDMs1/all_test"  \
-  --output_dir="/home/minju/PCDMs/output_ori(source)" \
+  --output_dir="/home/minju/PCDMs1/output_source" \
   --img_height=512  \
   --img_width=512   \
-  --learning_rate=1e-5 \
-  --train_batch_size=8 \
-  --max_train_steps=30000 \
+  --learning_rate=1e-4 \
+  --train_batch_size=4 \
+  --max_train_steps=25000 \
   --mixed_precision="fp16" \
   --checkpointing_steps=5000  \
   --noise_offset=0.1 \
   --lr_warmup_steps 5000  \
   --seed 42 \
   --report_to wandb \
-  --project_name=Source
+  --project_name=PCDMs_Source
+
+
+
--- run_test_stage2.sh
+++ run_test_stage2.sh
@@ -1,12 +1,11 @@
-python3  stage2_batchtest_inpaint_model.py \
+CUDA_VISIBLE_DEVICES=0 python3 stage2_test_recursive_randomcolor.py \
   --img_weigh 512 \
   --img_height 512 \
- --pretrained_model_name_or_path="stabilityai/stable-diffusion-2-1-base" \
- --image_encoder_p_path='facebook/dinov2-giant' \
- --img_path='{image_path}' \
- --json_path='{data.json}' \
- --pose_path="{pose_path}" \
- --target_embed_path="./logs/view_stage1/512_512/" \
- --save_path="./logs/view_stage2/512_512" \
- --weights_name="{save_ckpt}" \
- --calculate_metrics
+  --pretrained_model_name_or_path="stabilityai/stable-diffusion-2-1-base" \
+  --image_encoder_p_path='facebook/dinov2-giant' \
+  --img_path='/home/minju/PCDMs/test_data/' \
+  --json_path='/home/minju/PCDMs/data/test.json' \
+  --save_path="/home/minju/PCDMs/result/gt_cond_recursive" \
+  --weights_name="/home/minju/PCDMs/output_randomre/25000" \
+  --calculate_metrics
+
